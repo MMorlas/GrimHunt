96a81fc324357ffa9c25bcb984b0b314225571c7
MMorlas <marcmorlas2018@cev.com>
2019-11-11 12:03:04 +0100

    a

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+	Rigidbody myBody;
+	private float lifeTimer = 2f;
+	private float timer;
+	private bool hitSomething = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+        transform.rotation = Quaternion.LookRotation(myBody.velocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(!hitSomething)
+		{
+			transform.rotation = Quaternion.LookRotation(myBody.velocity);
+		}
+
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		hitSomething = true;
+		Stick();
+	}
+
+	private void Stick()
+	{
+		myBody.constraints = RigidbodyConstraints.FreezeAll;
+	}
+}
+
+
